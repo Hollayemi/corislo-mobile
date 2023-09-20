4d8102a6d3770e4fc6d82ce92f07934209b7b1a9
deralph <jraphael441@gmail.com>
2023-09-20 08:12:18 -0700

    added useswr and axios

--- App.tsx
+++ App.tsx
@@ -2,11 +2,15 @@ import * as React from "react";
 import Navigation from "./navigations/stackNavigation";
 import { Provider } from "react-redux";
 import store from "./store";
+import { SWRConfig } from "swr";
+import { fetcher } from "./services/axios/fetcher";
 
 export default function Main() {
   return (
     <Provider store={store}>
-      <Navigation />
+      <SWRConfig value={{ fetcher }}>
+        <Navigation />
+      </SWRConfig>
     </Provider>
   );
 }
